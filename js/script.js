ce932cee3cfa8dab1a1ae5f119db882c51847d41
AnonDEV23 <luisayvalballesteros@icloud.com>
2023-06-20 20:45:31 -0500

    lol

--- js/script.js
+++ js/script.js
@@ -1,59 +1,73 @@
-// Wait for the DOM to fully load
-document.addEventListener("DOMContentLoaded", () => {
-    const contactForm = document.querySelector("#contact-form");
+
+const btnEnd = document.getElementById('btn-end')
+const form = document.getElementById('contact-form')
+const contactBtn = document.getElementById('contact')
+let toogle = true 
+
+contactBtn.addEventListener('click', () => {
+  toogle = !toogle
+
+  if (toogle == true) {
+    form.style.display = 'none'
+  }
+
+  if (toogle == false) {
+    form.style.display = 'grid'
+  }
+})
+
+btnEnd.addEventListener('click', () => {
+  toogle = !toogle
+
+  if (toogle == true) {
+    form.style.display = 'none'
+  }
+
+  if (toogle == false) {
+    form.style.display = 'grid'
+  }
+})
+
+boton.addEventListener('click', function() { toogle = !toogle 
+  if(toogle === true ){
+      body.style.background = 'white'
+          products.style.background = 'gray'
+          p.style.color = 'black'
+          h3.style.color = 'black'
+          h31.style.color = `black`
+          label.style.color = 'black'
+  }
+  if(toogle === false) {
+      body.style.background = '#18100c'
+          products.style.background = '#000000'
+          p.style.color = 'white'
+          h3.style.color = 'white'
+          h31.style.color = `white`
+          label.style.color = 'white'
+  }
+} )
+
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault(); 
   
-    // Handle form submit event
-    contactForm.addEventListener("submit", (event) => {
-      event.preventDefault();
-  
-      const nameInput = document.querySelector("#name");
-      const messageInput = document.querySelector("#message");
-  
-      // Send form data to server
-      fetch("https://example.com/contact", {
-        method: "POST",
-        body: JSON.stringify({
-          name: nameInput.value,
-          message: messageInput.value,
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-        .then((response) => {
-          // Handle response from server
-          if (response.ok) {
-            alert("Form submitted successfully!");
-            nameInput.value = "";
-            messageInput.value = "";
-          } else {
-            alert("Form submission failed.");
-          }
-        })
-        .catch((error) => {
-          console.error("Error submitting form: ", error);
-          alert("Form submission failed.");
-        });
-    });
-  
-    const contactButton = document.querySelector("#contact");
-  
-    // Open contact form when contact button is clicked
-    contactButton.addEventListener("click", (event) => {
-      event.preventDefault();
-  
-      const contactForm = document.querySelector("#contact-form");
-      contactForm.classList.toggle("hidden");
-    });
-  
-    const closeButton = document.querySelector("#btn-end");
-  
-    // Close contact form when close button is clicked
-    closeButton.addEventListener("click", (event) => {
-      event.preventDefault();
-  
-      const contactForm = document.querySelector("#contact-form");
-      contactForm.classList.toggle("hidden");
-    });
+    var name = document.getElementById("name").value;
+    var email = "dylanp.arango12@gmail.com";
+    var message = document.getElementById("message").value;
+
+    function enviarCorreo(destinatario, asunto, cuerpo) {
+        const correo = `mailto:${destinatario}?subject=${encodeURIComponent(asunto)}&body=${encodeURIComponent(cuerpo)}`;
+        window.open(correo);
+      }
+
+    if (!name || !email || !message) {
+      alert("Por favor, complete todos los campos.");
+      return;
+    }
+
+    alert("Abriendo gmail para enviar tu mensaje...");
+    document.getElementById("contact-form").reset();
+    
+    enviarCorreo(email, name, message);
+    
   });
-  
+
